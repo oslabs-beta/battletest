0f9d1bd292c690e4f057ddbd13ef30067aee602e
Soobin Kim <soobinkim1@gmail.com>
2020-04-10 09:59:38 -0400

    updated writeTest; skeleton for parseConfig; added requireUncached

---
+++ bin/cmds/generate.js
@@ -0,0 +1,14 @@
+/**
+ * @name bin/cmds/generate
+ * @description execute "battletest generate" to create test files for all or specified routes based on battletest.config.js
+ * @param {Array.String} [routes] - names of routes for which test files will be generated, specified by the user
+ * @returns {null} undefined
+ */
+const requireUncached = require('../util/requireUncached.js');
+
+const generate = (routes) => {
+    
+    // TO DO - if any specified route is not in .config, return error & stop execution
+}
+
+module.exports = generate;
