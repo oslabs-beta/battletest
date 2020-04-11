c130f9aee2d5a566133f49a2f18cf9f7f4673574
Soobin Kim <soobinkim1@gmail.com>
2020-04-11 11:34:01 -0400

    fixes to cli

--- bin/cli.js
+++ bin/cli.js
@@ -1,16 +1,18 @@
 #!/usr/bin/env node
-// console.log('hello world');
-// process.exit();
+
+/**
+ * @name cli.js
+ * @description execute varous command line arguments
+ */
 
 //  process.argv holds the arguments from when we run battletest from the command line
 //  e.g. running $battletest init would result in process.argv having ['/node','/battletest', 'init']
 const args = process.argv.slice(2);
-//  the above line slices the first 2 extra arguments, results in an array of cmds ['init']
-
-// ADD LOGIC TO HANDLE FLAGS (--save, etc.)
+//  the above line slices the first 2 extra arguments, results in an array of cmds ['init', ...]
 
 const cmd = args[0] || 'help';
 // cmd equals first argument in array, othewise will equal 'help'
+
 
 switch (cmd) {
   case 'help' || '-help' || '--help' || '-h':
@@ -33,13 +35,18 @@ switch (cmd) {
       console.error(`battletest: "${cmd}" does not take in additional arguments.  Please see "battletest --help" for available commands.`)
     } 
     else {
-    require('./cmds/init')();
+      require('./cmds/init')();
     }
     break;
   case 'generate':
+    // TO DO: "generate" should take in arguments to specify which test files to generate
     require('./cmds/generate')();
     break;
+  case 'start':
+    const testFiles = args.slice(1)
+    require('./cmds/start')(...testFiles);
+    break;
   default:
-    console.error(`"${cmd}" is not a valid command`);
+    console.error(`battletest: "${cmd}" is not a valid command. Please see "battletest --help" for available commands.`);
     break;
 }
