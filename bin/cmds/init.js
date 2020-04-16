6477aa31a960aa9e126edb70fbc21567f718d358
Soobin Kim <soobinkim1@gmail.com>
2020-04-16 12:30:34 -0400

    init.js can take in arguments and call appropriate parsers

--- bin/cmds/init.js
+++ bin/cmds/init.js
@@ -2,21 +2,52 @@
  * @name bin/cmds/init
  * @description execute "battletest init" by creating "__battletest__" folder and battletest.config.js
  * @param {null} undefined
- * 
+ *
  */
 
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
+const saveConfigFile = require("../src/init/saveConfigFile.js");
+const checkFiles = require("../util/checkFiles.js");
+// const expressParser = require("../src/init/expressParser.js");
+// const openApiParser = require("../src/init/openApiParser.js");
 
-const configFile = ``
+const init = (args) => {
+  // if no args specified, provide empty config
+  if (args.length === 0) {
+    const configFile = require("../src/init/emptyConfig.js");
+    saveConfigFile(configFile);
+    return;
+  }
+  if (args.length > 1) {
+    console.log(
+      `battletest: \n Please specify only one source file location to generate battletest.config.js.  \n Currently accepted source files are: \n - openAPI file in .yaml or .json \n - express-js framework server.js file that exports app/express() object`
+    );
+    return;
+  }
 
-const init = () => {
-    // 
-    // Save battletest.config.js to root directory.
-    const configPath = path.resolve(process.cwd(), 'battletest.config.js')
-    fs.writeFile(configPath, configFile, function(err) {
-        if (err) throw err
-    })
+  // check if file exists
+  const fileLocation = args[0];
+  const notInDirectory = checkFiles([fileLocation], process.cwd());
+  if (notInDirectory.length > 0) {
+    console.log(`battletest: '${notInDirectory[0]}' was not found.`);
+    return;
+  }
+
+  const fileType = fileLocation.split(".")[1];
+  if (fileType === "js") {
+    // check if it is an express file
+    if ()
+
+    //const configFile = expressParser(fileLocation);
+    return;
+  }
+
+  if (fileType === "json" || fileType === "yaml") {
+    // const configFile = expressParser(fileLocation);
+  }
+
+  console.log(`battletest`)
 };
 
 module.exports = init;
--- bin/cmds/start.js
+++ bin/cmds/start.js
@@ -5,29 +5,33 @@
  * @returns {null} undefined
  */
 
-const fs = require('fs');
-const path = require('path');
-const Mocha = require('mocha');
-
-const mochaOptions = require('../src/mochaOptions.js');
-const checkFiles = require('../src/start/checkFiles.js')
+const fs = require("fs");
+const path = require("path");
+const Mocha = require("mocha");
+const mochaOptions = require("../src/mochaOptions.js");
+const checkFiles = require("../util/checkFiles.js");
 
 const mocha = new Mocha(mochaOptions);
 
 const start = (...args) => {
-  const directory = path.resolve(process.cwd(), '__battletest__');
+  const directory = path.resolve(process.cwd(), "__battletest__");
 
   if (args.length > 0) {
     const notInTestDirectory = checkFiles(args, directory);
     if (notInTestDirectory.length > 0) {
-      console.error(`battletest: following files were not found in __battletest__: ${notInTestDirectory}.`);
-      return; 
+      console.error(
+        `battletest: following files were not found in __battletest__: ${notInTestDirectory}.`
+      );
+      return;
     }
-    if (args.indexOf('testSetup.js') < 0) { args.push('testSetup.js'); }   
-  } 
-  else {
+    if (args.indexOf("testSetup.js") < 0) {
+      args.push("testSetup.js");
+    }
+  } else {
     // queue all .js files in __battletest__ to be executed.
-    args = fs.readdirSync(directory).filter((filename) => filename.substr(-3) === '.js');
+    args = fs
+      .readdirSync(directory)
+      .filter((filename) => filename.substr(-3) === ".js");
   }
   // TO DO: Ensure testSetup has been created
   // add all files to the mocha instance to be executed.
