18fcefc8ee2befaae04f5993ce98383f616dc914
Kevin Luo <kevinluo@Kevins-MBP-2.home>
2020-04-27 15:49:02 -0400

    integration tests started

---
+++ test/init.test.js
@@ -0,0 +1,10 @@
+const { expect } = require('chai');
+const init = require('../lib/cli/cmds/init');
+/**
+ * Integration testing for the init command
+ */
+
+
+describe('Integration testing: init', () => {
+  describe('Base/Edge case arguments');
+});
