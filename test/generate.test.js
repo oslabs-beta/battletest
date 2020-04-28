5ca6c9f1c51dd26c64b36658227fc1f90a2b7a88
Kevin Luo <kevinluo@Kevins-MBP-2.home>
2020-04-28 11:30:38 -0400

    small fix

--- test/generate.test.js
+++ test/generate.test.js
@@ -6,11 +6,13 @@ const fs = require('fs');
 const generate = require('../lib/cli/cmds/generate.js');
 
 describe('Integration testing: generate', () => {
-  beforeEach('deletes any previously created filesystem() => {
+  beforeEach('deletes any previously created filesystem', () => {
     if (fs.existsSync('__battletest__')) fs.unlinkSync('__battletest__');
   });
   const config = require('./resources/battletest.config.js');
   describe('Base/edge cases', () => {
-    it('should ');
+    it('should ', () => {
+
+    });
   });
 });
